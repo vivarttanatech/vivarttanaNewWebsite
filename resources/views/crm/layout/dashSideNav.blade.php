4ba9e43bfd9680726cddd8f4d531f1ce3b7d5ad3
Jacob Dennis P <jacoden143@gmail.com>
2021-11-04 22:53:46 +0530

    modified side nav

--- resources/views/crm/layout/dashSideNav.blade.php
+++ resources/views/crm/layout/dashSideNav.blade.php
@@ -1,36 +1,24 @@
-<div class="p-3 text-white bg-dark" style="width: 250px; height:750px">
+  <div class="p-3 text-white bg-dark" style="width: 250px; height:750px">
     <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
-          Home
-        </a>
-      </li>
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
           Dashboard
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
           Orders
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
           Products
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
           Customers
         </a>
       </li>
     </ul>
-    
-    
   </div>
