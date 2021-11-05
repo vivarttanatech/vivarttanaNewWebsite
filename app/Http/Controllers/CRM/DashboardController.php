614353154f3f5f1c1f23639d8a66c57e7fc43856
Jacob Dennis P <jacoden143@gmail.com>
2021-11-05 17:08:32 +0530

    development started

--- app/Http/Controllers/CRM/DashboardController.php
+++ app/Http/Controllers/CRM/DashboardController.php
@@ -25,21 +25,27 @@ class DashboardController extends Controller
     public function ClassDelete(){}
     public function ClassHistory(){}
     
-    public function CourseListing(){}
+    public function CourseListing(){
+        return view('crm.admin.courses.CourseListingView');
+    }
     public function CourseAdd(){}
     public function CourseSave(){}
     public function CourseEdit(){}
     public function CourseUpdate(){}
     public function CourseDelete(){}
 
-    public function CurriculumListing(){}
+    public function CurriculumListing(){
+        return view('crm.admin.curriculum.CurriculumListingView');
+    }
     public function CurriculumAdd(){}
     public function CurriculumSave(){}
     public function CurriculumEdit(){}
     public function CurriculumUpdate(){}
     public function CurriculumDelete(){}
 
-    public function CollegeListing(){}
+    public function CollegeListing(){
+        return view('crm.admin.college.CollegeListingView');
+    }
     public function CollegeAdd(){}
     public function CollegeSave(){}
     public function CollegeEdit(){}
