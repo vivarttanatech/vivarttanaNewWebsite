f9bdd323cc02fc4ee1c3da55b0669e50c65ac9e4
Jacob Dennis P <jacoden143@gmail.com>
2021-11-05 11:54:07 +0530

    bug fix

--- resources/views/crm/layout/dashSideNav.blade.php
+++ resources/views/crm/layout/dashSideNav.blade.php
@@ -1,4 +1,14 @@
-  <div class="p-3 text-white bg-dark" style="width: 250px; height:750px">
+  <style>
+    .sidebar li .submenu{ 
+    list-style: none; 
+    margin: 0; 
+    padding: 0; 
+    padding-left: 1rem; 
+    padding-right: 1rem;
+    color:white;
+    }
+  </style>
+  <div class="p-3 text-white bg-dark sidebar" style="width: 250px; height:750px">
     <ul class="nav nav-pills flex-column mb-auto">
       <li>
         <a href="#" class="nav-link text-white">
@@ -6,19 +16,77 @@
         </a>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
-          Orders
-        </a>
+        <a class="nav-link text-white" href="#"> Users </a>
+        <ul class="submenu collapse">
+          <li><a class="nav-link text-white" href="#">Students </a></li>
+          <li><a class="nav-link text-white" href="#">Trainers </a></li>
+          <li><a class="nav-link text-white"  href="#">CRM Users</a></li>
+        </ul>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
-          Products
-        </a>
+        <a class="nav-link text-white" href="#">Classes</a>
+        <ul class="submenu collapse">
+          <li><a class="nav-link text-white" href="#">Class Management</a></li>
+          <li><a class="nav-link text-white" href="#">Upcoming Classes</a></li>
+          <li><a class="nav-link text-white" href="#">Ongoing Classes</a></li>
+          <li><a class="nav-link text-white"  href="#">History</a></li>
+        </ul>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
-          Customers
-        </a>
+        <a class="nav-link text-white" href="#">Courses</a>
+      </li>
+      <li>
+        <a class="nav-link text-white" href="#">Curriculum</a>
+      </li>
+    </ul>
+
+    <ul class="nav nav-pills flex-column">
+      <li>
+        <a class="nav-link text-white" href="#">College</a>
+        <ul class="submenu collapse">
+          <li><a class="nav-link text-white" href="#">College Management</a></li>
+          <li><a class="nav-link text-white" href="#">Degree</a></li>
+          <li><a class="nav-link text-white" href="#">Degree Branch</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <ul class="nav nav-pills flex-column pt-5">
+      <li>
+        <a class="nav-link text-white" href="#">Settings</a>
+        <ul class="submenu collapse">
+          <li><a class="nav-link text-white" href="#">CRM Settings</a></li>
+        </ul>
       </li>
     </ul>
   </div>
+  <script>
+    document.addEventListener("DOMContentLoaded", function(){
+  document.querySelectorAll('.sidebar .nav-link').forEach(function(element){
+    
+    element.addEventListener('click', function (e) {
+
+      let nextEl = element.nextElementSibling;
+      let parentEl  = element.parentElement;	
+
+        if(nextEl) {
+            e.preventDefault();	
+            let mycollapse = new bootstrap.Collapse(nextEl);
+            
+            if(nextEl.classList.contains('show')){
+              mycollapse.hide();
+            } else {
+                mycollapse.show();
+                // find other submenus with class=show
+                var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
+                // if it exists, then close all of them
+                if(opened_submenu){
+                  new bootstrap.Collapse(opened_submenu);
+                }
+            }
+        }
+    }); // addEventListener
+  }) // forEach
+}); 
+// DOMContentLoaded  end
+  </script>
--- resources/views/crm/layout/dashTopNav.blade.php
+++ resources/views/crm/layout/dashTopNav.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#">Vivarttana LMS</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -11,13 +11,10 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                        <strong>mdo</strong>
+                        <strong>{{Auth()->user()->name}}</strong>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                        <li><hr class="dropdown-divider"></li>
+                      
                         @yield('logouturl')
                     </ul>
                 </li>
